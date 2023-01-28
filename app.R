ccd6638b42847c1a1a00ad299c203d1f1bac5e55
Mathieu Pélissié <mathieu.pelissie@ens-lyon.fr>
2023-01-28 18:34:45 +0100

    Occurrence file splitted

--- app.R
+++ app.R
@@ -5,12 +5,18 @@ library(tidyverse)
 
 # Occurrence --------------------------------------------------------------
 
-# occ <- readr::read_csv("Code/Artportalen_odonate_maps/Odonata_Artportalen_2006.2020_flt.wgs84_thin.csv")
-occ2 <- readr::read_csv("Code/Artportalen_odonate_maps/Odonata_Artportalen_2006.2020_flt.wgs84_linux.csv") %>% 
+# occ <- readr::read_csv("Data/Occurrence/Odonata_Artportalen_2006.2020_flt.wgs84_thin.csv")
+occ2 <- readr::read_csv("Data/Occurrence/Odonata_Artportalen_2006.2020_flt.wgs84_linux.csv") %>% 
   dplyr::filter(Noggrannhet<=1000) %>% 
   dplyr::select(Species, YEAR, Longitude, Latitude, Obsid)
 
-occ_thin <- readr::read_csv("Code/Artportalen_odonate_maps/thinned_10km.csv")
+occ2 <- readr::read_csv("Data/Occurrence/Odonata_Artportalen_2006.2020_flt.wgs84_linux(1).csv") %>% # raw file splitted to be less than 50Mo individually
+  rbind(readr::read_csv("Data/Occurrence/Odonata_Artportalen_2006.2020_flt.wgs84_linux(2).csv")) %>% 
+  dplyr::filter(Noggrannhet<=1000) %>% 
+  dplyr::select(Species, YEAR, Longitude, Latitude, Obsid)
+
+
+occ_thin <- readr::read_csv("Data/Occurrence/thinned_10km.csv")
 spe <- unique(occ_thin$Species)
 spe <- spe[!spe %in% c("Aeshna affinis","Anax ephippiger","Nehalennia speciosa")]
 
